6fb9098975d81f2cccd72ee4eb9a62eeb2179741
Rajtamakuwala3 <98575151+Rajtamakuwala3@users.noreply.github.com>
2024-07-31 00:02:59 +0530

    Implemented @BeforeEach and @AfterEach method to initialize class before and after test.

--- src/test/java/com.testStringCalcTDD/StringCalculatorTest.java
+++ src/test/java/com.testStringCalcTDD/StringCalculatorTest.java
@@ -1,6 +1,20 @@
 package com.testStringCalcTDD;
 
-public class StringCalculatorTest {
+import com.stringCalcTDD.StringCalculator;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 
+public class StringCalculatorTest {
+    StringCalculator stringCalculator;
+
+    @BeforeEach
+    public void init(){
+        stringCalculator = new StringCalculator();
+    }
+
+    @AfterEach
+    public void destroy() {
+        stringCalculator = null;
+    }
 
 }
